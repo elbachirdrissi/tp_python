5a07df84e2f07482084ee21dcb7006d9603aedb7
El Bachir <elbachir.drissi2003@gmail.com>
2023-10-16 15:10:05 +0100

    version_finale_ex_9

--- tp1/ex9_v2.py
+++ tp1/ex9_v2.py
@@ -2,7 +2,9 @@ nbr_article = int(input("Combien d'article vous avez ? "))
 liste_nom = []
 liste_qte = []
 liste_prix = []
-liste_total = []
+total_ht = []
+total_ttc = []
+som_ttc = 0
 
 for i in range(nbr_article):
     nom = input(f"Donner le nom de l'article {i+1}: ")
@@ -12,11 +14,14 @@ for i in range(nbr_article):
     prix = float(input(f"Donner le prix unitaire de l'article {i+1}: "))
     liste_prix.append(prix)
     total = prix * qte
-    liste_total.append(total)
+    total_ht.append(total)
+    ttc = total + total*0.2
+    total_ttc.append(ttc)
    
-for prix in liste_total:
+for prix in total_ht:
     print(f"Total pour l'article {i+1}: {prix} dh (HT)")
 
+for i in total_ttc:
+    som_ttc += i
 
-'''total = (total_1 + total_1*0.2) + (total_2 + total_2*0.2)
-print(f"Le total de votre facture est: {total} dh (TTC)")'''
+print(f"Le total de votre facture est:: {som_ttc} dh (TTC)")
